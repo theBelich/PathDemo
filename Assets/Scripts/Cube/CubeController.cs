0e9304307c2f6fa418242190c4c83073fb754632
Belich <43599300+Chopperl@users.noreply.github.com>
2022-11-01 15:14:13 +0400

    spawns obstacles and fix targeted plane for yellow

--- Assets/Scripts/Cube/CubeController.cs
+++ Assets/Scripts/Cube/CubeController.cs
@@ -6,16 +6,25 @@ using UnityEngine.AI;
 public class CubeController : MonoBehaviour
 {
     [SerializeField] private Transform _targetDestination;
+    [SerializeField] private RandomizePath _controller;
     private NavMeshAgent agent;
+    private Vector3 startPosition;
 
     // Start is called before the first frame update
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        startPosition = gameObject.transform.position;
     }
 
     private void FixedUpdate()
     {
         agent.SetDestination(_targetDestination.position);
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        _controller.RandomizePathLength();
+        gameObject.transform. position = startPosition;
+    }
+
 }
--- Assets/Scripts/Enviroment/RandomizePath.cs
+++ Assets/Scripts/Enviroment/RandomizePath.cs
@@ -11,6 +11,7 @@ public class RandomizePath : MonoBehaviour
     void Start()
     {
         RandomizePathLength();
+        SpawnObstacle();
     }
 
     // Update is called once per frame
@@ -28,5 +29,16 @@ public class RandomizePath : MonoBehaviour
         x = Random.Range(0, 10);
         z = Random.Range(8.4f, 20);
         _greenTarget.transform.position = new Vector3(x, 0, z);
+    }
+
+    public void SpawnObstacle()
+    {
+        float x = _yellowTarget.transform.position.x - Random.Range(3, 7);
+        float z = _yellowTarget.transform.position.z - Random.Range(3, 7);
+        Instantiate(_obstaclePrefab, new Vector3(x, 0, z), Quaternion.identity);
+
+        x = _greenTarget.transform.position.x - Random.Range(1, 5);
+        z = _greenTarget.transform.position.z - Random.Range(1, 5);
+        Instantiate(_obstaclePrefab, new Vector3(x, 0, z), Quaternion.identity);
     }
 }
